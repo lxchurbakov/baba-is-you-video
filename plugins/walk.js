bda166d6a134200d84193ab1d2dfb64d16c6fbe2
lxchurbakov <lxchurbakov@gmail.com>
2024-07-09 13:37:23 +0300

    Make baba walk

--- plugins/walk.js
+++ plugins/walk.js
@@ -1,5 +1,12 @@
 import baba from '../assets/baba.png';
 
+const UP_KEY = 87;
+const DOWN_KEY = 83;
+const LEFT_KEY = 65;
+const RIGHT_KEY = 68;
+
+const GRID_SIZE = 60;
+
 const loadImage = async (url) => new Promise((resolve, reject) => {
     const img = new Image();
 
@@ -21,10 +28,33 @@ export class Walk {
 
         context.scale(pixelRatio, pixelRatio);
 
-        this.loadAndSetupRender(context, rect);
+        this.position = { x: 0, y: 0 };
+
+        this.setupRender(context, rect);
+        this.setupKeyEvents();
     }
 
-    loadAndSetupRender = (context, rect) => {
+    setupKeyEvents = () => {
+        window.addEventListener('keydown', (e) => {
+            if (e.keyCode === UP_KEY) {
+                this.position.y -= 1;
+            }
+
+            if (e.keyCode === DOWN_KEY) {
+                this.position.y += 1;
+            }
+
+            if (e.keyCode === RIGHT_KEY) {
+                this.position.x += 1;
+            }
+
+            if (e.keyCode === LEFT_KEY) {
+                this.position.x -= 1;
+            }
+        });
+    };
+
+    setupRender = (context, rect) => {
         let img = null;
 
         loadImage(baba).then((i) => {
@@ -36,15 +66,11 @@ export class Walk {
         const render = () => {
             context.clearRect(0, 0, rect.width, rect.height);
 
-            const center = { x: rect.width / 2, y: rect.height / 2 };
-
-            const pos = center;
-            const GRID_SIZE = 100;
-
             if (img) {
                 const size = { x: img.width * 9, y: img.height * 9 };
+                const position = { x: this.position.x * GRID_SIZE, y: this.position.y * GRID_SIZE };
 
-                context.drawImage(img, pos.x + GRID_SIZE / 2 - size.x / 2, pos.y + GRID_SIZE - size.y, size.x, size.y);
+                context.drawImage(img, position.x + GRID_SIZE / 2 - size.x / 2, position.y + GRID_SIZE - size.y, size.x, size.y);
             }
 
             requestAnimationFrame(render);
