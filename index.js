c1e26a0ec31f5149248ba6babe7700796254d6ce
lxchurbakov <lxchurbakov@gmail.com>
2024-07-09 12:02:12 +0300

    Add simple plugin and complete starter

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//
+import { HelloWorld, HelloWorld } from './plugins/hello-world';
+
+const canvas = document.getElementById('app');
+
+if (!canvas) {
+    throw new Error('canvas isnt there!');
+}
+
+const HelloWorld = new HelloWorld(canvas);
